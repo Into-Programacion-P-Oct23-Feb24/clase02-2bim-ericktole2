b0000159582b67369fe46e9a8d86e117645d53b2
estoledo <estoledo1@utpl.edu.ec>
2023-12-13 15:58:53 -0500

    ejemplos

--- Ejemplos/src/paquete1/Ejemplo03.java
+++ Ejemplos/src/paquete1/Ejemplo03.java
@@ -18,7 +18,7 @@ public class Ejemplo03 {
         // Declarar y generar arreglos en Java
 
         // arreglo de tipo entero
-        int[] arreglo1 = new int[5];
+        int[] arreglo1 = new int[5];//la manera mas formal para poder declarar un arreglo 
         
         // arreglo de tipo entero
         int[] arreglo2;
